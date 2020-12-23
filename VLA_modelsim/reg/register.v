37c5d6083a55dedc8ffba4a67275811c5e85a057
Quang Phan <quangphan240599@gmail.com>
2020-12-23 02:12:09 +0200

    Create simple register

---
+++ VLA_modelsim/reg/register.v
@@ -0,0 +1,16 @@
+module register #(parameter WIDTH=8)
+   (input wire [WIDTH-1:0] data_in,
+    input wire 		   load,clk,rst,
+    output reg [WIDTH-1:0] data_out);
+
+   always @(posedge clk)
+     begin
+	if (rst == 1)
+	  data_out <= WIDTH'('b0);
+	else
+	  data_out <= load ? data_in : data_out;
+     end
+
+endmodule; // register
+
+ 
