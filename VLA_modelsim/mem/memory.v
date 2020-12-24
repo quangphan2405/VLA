b1f93fc05be17752e1385286c48e1fab6c566cac
Quang Phan <quangphan240599@gmail.com>
2020-12-24 03:14:34 +0200

    Using exponential instead of localparam

--- VLA_modelsim/mem/memory.v
+++ VLA_modelsim/mem/memory.v
@@ -2,10 +2,8 @@ module memory #(parameter integer AWIDTH = 5, DWIDTH = 8)
    (input [AWIDTH-1:0] addr,
     input 	       clk, wr, rd,
     inout [DWIDTH-1:0] data);
-
-   localparam MAXSIZE = {AWIDTH{1'b1}};
    
-   reg [DWIDTH-1:0]    mem[MAXSIZE:0];  
+   reg [DWIDTH-1:0]    mem[0:2**AWIDTH-1];  
    
    always @(posedge clk)
      begin	
